ab4f3de5fcee56c78b6f75c2f273b221c89dc821
FreddyNietz <junelaljei@gmail.com>
2020-10-05 21:55:31 +0800

    Added a server on junnie.js

--- junnie.js
+++ junnie.js
@@ -1,1 +1,28 @@
-console.log("Hello World!")
+const Fastify = require("fastify");
+
+
+const server = Fastify({
+    logger: true,
+    trustProxy: true
+});
+
+
+server.get('/',{
+  
+    handler: async (req) => {
+        console.log(req);
+        return {success:true}
+    }
+})
+
+
+async function start() {
+    const port = parseInt(process.env.PORT || '8080');
+    const address = "0.0.0.0";
+
+    const addr = await server.listen(port,address);
+    console.log(`Listening on ${addr}`);
+}
+
+
+start();

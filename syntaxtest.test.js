44f473405326cf00c559da8edd6947dde529be10
FreddyNietz <junelaljei@gmail.com>
2020-10-26 20:53:48 +0800

    Add a test case

---
+++ syntaxtest.test.js
@@ -0,0 +1,31 @@
+const { build } = require('./junnie');
+
+require('tap').mochaGlobals();
+require('should');
+
+
+describe('for the route for (/)', 
+
+() =>
+{
+    let app;
+    before(async() =>
+    {
+        app = await build();
+    });
+    it('it should return {success:true} with method GET',async() =>
+    {
+        const response = await app.inject({
+            method: 'GET',
+            url:'/'
+        });
+        const payload = response.json();
+        const {statusCode} = app;
+        const {success} = payload;
+        success.should.equal(true);
+        console.log('payload:',payload);
+    });
+    
+} 
+
+);

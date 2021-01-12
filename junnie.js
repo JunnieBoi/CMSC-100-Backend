60f6fa2ab0a6d30f69b9493ad5f0d8e9252fa34a
FreddyNietz <junelaljei@gmail.com>
2021-01-12 16:16:24 +0800

    add fastify swagger

--- junnie.js
+++ junnie.js
@@ -1,10 +1,29 @@
 const Fastify = require("fastify");
 const {routes} = require('./route');
 const { connect } = require('./db');
+const swagger = require('fastify-swagger');
+const { definitions } = require('./definitions');
+const { name: title, description, version } = require('./package.json');
 
 exports.build = async(opts = {logger:false,trustProxy:false}) =>
 {
     const app = Fastify(opts);
+    app.register(swagger, {
+        routePrefix: '/docs',
+        exposeRoute: true,
+        swagger: {
+          info: {
+            title,
+            description,
+            version
+          },
+          schemes: ['http', 'https'],
+          consumes: ['application/json'],
+          produces: ['application/json'],
+          definitions
+        }
+      });
+    
     await connect();
     routes(app);
     return app;

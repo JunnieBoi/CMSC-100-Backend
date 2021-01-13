e9aba8a139117171e71b3cd953c1c35a3e259c9e
FreddyNietz <junelaljei@gmail.com>
2021-01-13 19:11:30 +0800

    add user create and login

---
+++ db/models/user.js
@@ -0,0 +1,30 @@
+module.exports = (mongoose) => {
+    const { Schema } = mongoose;
+  
+    const userSchema = new Schema({
+      username: {
+        type: String,
+        required: true,
+        immutable: true,
+        index: true,
+        unique: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      dateCreated: {
+        type: Number,
+        required: true,
+        default: () => new Date().getTime()
+      },
+      dateUpdated: {
+        type: Number,
+        required: true,
+        default: () => new Date().getTime()
+      }
+    });
+  
+    return mongoose.model('User', userSchema);
+  };
+  

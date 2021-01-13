aee117b8de1eed64b68af9eb1c6ab63fba5fdd65
FreddyNietz <junelaljei@gmail.com>
2021-01-13 21:30:44 +0800

    add jwt registration

--- junnie.js
+++ junnie.js
@@ -6,6 +6,12 @@ const { definitions } = require('./definitions');
 const { name: title, description, version } = require('./package.json');
 const sensible = require('fastify-sensible');
 const { errorHandler } = require('./error-handler');
+const jwt = require('fastify-jwt');
+const { readFileSync } = require('fs');
+
+
+const audience = 'this-audience';
+const issuer = 'localhost';
 
 
 exports.build = async(opts = {logger:false,trustProxy:false}) =>
@@ -15,6 +21,24 @@ exports.build = async(opts = {logger:false,trustProxy:false}) =>
     app.register(sensible).after(() => {
       app.setErrorHandler(errorHandler);
     });
+
+     app.register(jwt, {
+    secret: {
+      private: readFileSync('./cert/keyfile', 'utf8'),
+      public: readFileSync('./cert/keyfile.key.pub', 'utf8')
+    },
+    sign: {
+      algorithm: 'RS256',
+      audience,
+      issuer,
+      expiresIn: '1h'
+    },
+    verify: {
+      audience,
+      issuer
+    }
+  });
+
   
     app.register(swagger, {
         routePrefix: '/docs',
